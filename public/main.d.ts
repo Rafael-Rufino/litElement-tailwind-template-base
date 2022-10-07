805b31a2bc434f8fbfd97aaf98102f21cf7e039d
rafael-rufino <rafael.rufino@gazin.com.br>
2022-10-07 01:28:04 -0300

    feat:subindo template base para utilizar litelent + tailwind!

---
+++ public/main.d.ts
@@ -0,0 +1,6 @@
+import "./global/styles.css";
+import { LitElement } from "lit-element";
+export default class AppComponent extends LitElement {
+    createRenderRoot(): this;
+    render(): import("lit-html").TemplateResult<1>;
+}
